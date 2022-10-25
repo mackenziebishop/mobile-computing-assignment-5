b41389c39f3bd943db772a681817a904c5024f85
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-10-24 23:05:00 -0500

    Added UI Layout

---
+++ Bishop_WordGuess/Bishop_WordGuess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bishop_WordGuess
+//
+//  Created by Bishop,Mackenzie N on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
