0c8e7071411cd544f9cbc8d6d489e5067b4832cf
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-10-27 11:44:53 -0500

    Added connections

--- Bishop_WordGuess/Bishop_WordGuess/ViewController.swift
+++ Bishop_WordGuess/Bishop_WordGuess/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordsGuessedLabel: UILabel!
+    @IBOutlet weak var wordsRemainingLabel: UILabel!
+    @IBOutlet weak var totalWordsLabel: UILabel!
+    
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func guessLetterButtonPressed(_ sender: Any) {
+    }
+    @IBAction func playAgainButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
